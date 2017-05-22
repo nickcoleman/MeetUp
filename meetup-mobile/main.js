266823a7c14a42f0c1dc55a1bc30e7a3740b1c04
Nick <nick@itprojmgt.com>
2017-05-22 09:14:30 -0600

    adding react-navigation

--- meetup-mobile/main.js
+++ meetup-mobile/main.js
@@ -1,17 +1,38 @@
-import Expo from 'expo';
+import Expo, { AppLoading } from 'expo';
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Colors from './constants/Colors';
-
-import {
-  HomeScreen,
-} from './src/screens';
+import { cachedFonts } from './helper';
+import Root from './src/Root';
 
 EStyleSheet.build(Colors);
 
 class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  }
+
+  componentDidMount() {
+    this.loadAssestsAsync();
+  }
+
+  async loadAssestsAsync() {
+    const fontAssests = cachedFonts([
+      { montserrat: require('./assets/fonts/Montserrat-Regular.ttf') },
+      { montserratBold: require('./assets/fonts/Montserrat-Bold.ttf') },
+      { montserratLight: require('./assets/fonts/Montserrat-Light.ttf') },
+      { montserratMed: require('./assets/fonts/Montserrat-Medium.ttf') },
+    ]);
+
+    await Promise.all(fontAssests);
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
-    return <HomeScreen />;
+    if (!this.state.fontLoaded) {
+      return <AppLoading />;
+    }
+    return <Root />;
   }
 }
 
---
+++ meetup-mobile/src/screens/notifications/NotificationScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class NotificationScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>NotificationScreen Component</Text>
+      </View>
+    );
+  }
+}
+
+export default NotificationScreen;
